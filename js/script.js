81f6afc382bd9e58815d192ed8a0c1dc65b5d177
selda kabirnejat <seldaakabirnejat@gmail.com>
2024-05-30 01:02:55 +0300

    justSwipe

--- js/script.js
+++ js/script.js
@@ -55,20 +55,14 @@ const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
   
+  
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+ 
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+ 
 });
