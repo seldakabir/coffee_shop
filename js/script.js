213348ac3e913d2fb45050766131a06a5d25770d
selda kabirnejat <seldaakabirnejat@gmail.com>
2024-05-29 22:14:25 +0300

    clickEvent

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ import App from './product.js';
 
 // Selectors
 const main = document.querySelector('.main');
+const buttonHolder=document.getElementsByClassName("buttonholder")
+const textHolder=document.getElementsByClassName("textholder")
+const buttonsArray = Array.from(buttonHolder);
+const textHolders = Array.from(textHolder);
 
 App.renderAllProducts();
 
@@ -18,4 +22,53 @@ main.addEventListener('click', function (e) {
     return;
 
   App.openModal(product);
+});
+
+
+
+//add click event to each button of menu bar
+buttonsArray.forEach(button => {
+  button.addEventListener("click", () => {
+      const index = parseInt(button.getAttribute('data-index'), 10);
+
+     
+      if (index >= 0 && index < textHolders.length) {
+      
+        const textHolder = textHolders[index];
+        if (textHolder.style.display === "flex") {
+          
+            textHolder.style.display = "none";
+        } else {
+          
+            textHolders.forEach(holder => {
+                holder.style.display = "none";
+            });
+            textHolder.style.display = "flex";
+        }
+      }
+  })
+});
+
+//swiper of menu bar
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
