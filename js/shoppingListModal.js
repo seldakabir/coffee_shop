cb07cf5107cfce8fa5fc00d3029b00c5c70ff0a8
selda kabirnejat <seldaakabirnejat@gmail.com>
2024-06-03 12:53:00 +0300

    startToShowOrder

--- js/shoppingListModal.js
+++ js/shoppingListModal.js
@@ -4,7 +4,11 @@ import scrollFix from './replaceRemovedScrollBar.js';
 const btnOpenModal = document.querySelector('.open-shopping-list');
 const modal = document.querySelector('.shopping-list-modal');
 const overlay = document.querySelector('.shopping-list__overlay');
-
+const plusBtn=document.querySelector('.pluse-btn');
+const minesBtn=document.querySelector('.mines-btn');
+const orderCount=document.querySelector('.order-count');
+let count=0
+let userBasket=[]
 // Functions
 const closeModal = function () {
   document.body.classList.remove('disable-scroll');
@@ -43,3 +47,29 @@ btnOpenModal.addEventListener('click', function () {
 
   toggleModalBtn(this);
 });
+//increase order count
+plusBtn.addEventListener("click",()=>{
+
+ 
+    count++
+    orderCount.innerHTML=count
+  
+
+})
+//decrease order count
+minesBtn.addEventListener("click",()=>{
+
+if(count>0){
+  count--
+
+}
+ else{
+  count=0
+  
+ }
+ orderCount.innerHTML=count
+})
+
+//show order products
+
+
